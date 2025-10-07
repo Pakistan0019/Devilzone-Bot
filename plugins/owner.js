55de653681bf05e5a6ab84ca3f67d254c86a2576
Nakhatri0 <100998571+Pakistan0019@users.noreply.github.com>
2025-10-07 05:14:49 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -29,7 +29,7 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/yj7zp0.png' }, // Image URL from your request
+            image: { url: 'https://files.catbox.moe/shmet4.jpg' }, // Image URL from your request
             caption: `╭━━〔 ** 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Here is the owner details*
@@ -53,7 +53,7 @@ async (conn, mek, m, { from }) => {
 
         // Send audio as per your request
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/4fz6jh.mp3' }, // Audio URL
+            audio: { url: '' }, // Audio URL
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
